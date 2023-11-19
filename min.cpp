c554d19bad2dfab2bf0aaa08ff4d0f70e2d5867e
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-11-19 16:14:05 +0600

    Find min from 3 or more values

--- min.cpp
+++ min.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main()
 {
 
-      int a, b;
-      cin >> a >> b;
+      int a, b, c, d;
+      cin >> a >> b >> c >> d;
 
-      cout << min(a, b);
+      cout << min(a, b) << endl;
+
+      cout << min({a, b, c, d});
 
       return 0;
 }
